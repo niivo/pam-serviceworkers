c07ee144b99894aee309e9e2178af8b807c9cef1
niivo <isaacnewtoncapsherman@gmail.com>
2025-08-20 20:35:27 -0300

    atualicacao do sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,17 @@
 self.addEventListener("install", (event) =>{
     caches.open("asd").then((cache) =>{
         console.log("cahce opened")
-        cache.add("index.html")
+        cache.addAll([
+            "index.html", 
+            "manifest.json",
+            "script.js", 
+            "sw.js"]);
     });
+});
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+            return response || fetch(event.request);
+        });
+    );
 });
